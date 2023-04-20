2f2a300efb4a225cdc074a4af9aa32d6dc56e9db
Mehdi <SetiZ@users.noreply.github.com>
2023-04-20 11:57:24 +0000

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   )
 }
