8a2d2f9ec0f115a6778ec4115d25ee6a27da763c
Mehdi <SetiZ@users.noreply.github.com>
2023-09-14 22:10:59 +0000

    update favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: LayoutProps) {
         <title>Les Miches à Micha - Micro-fournil artisanal</title>
         <meta name="description" content="N’allez plus à la boulangerie – c’est elle qui vient à vous ! Livraison à domicile, commande personnalisée, conseils de dégustation, créativité. . . Ici, tout est fait main, à partir de matières premières bio, locales et nobles, pour vous proposer des pains savoureux, sains et originaux." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/miches_blanc_no_text.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Flex as={'main'}
