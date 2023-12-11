16140df931bf80c0e5e8670006a5224f70f284d6
Mehdi <SetiZ@users.noreply.github.com>
2023-12-11 12:39:43 +0000

    rebase + format

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,21 +4,31 @@ import { Heading, Text } from '@chakra-ui/react';
 import Router from 'next/router';
 
 export default function Home() {
-  // if (typeof window !== 'undefined') {
-  //   window.location.replace('/socials');
-  // }
+	// if (typeof window !== 'undefined') {
+	//   window.location.replace('/socials');
+	// }
 
-  return (
-    <Layout>
-      <ContainerBox>
-        <Heading as={'h1'} textAlign={'center'}>Les Miches à Micha</Heading>
-        <Text fontSize='2xl' textAlign={'center'}>Micro-fournil artisanal</Text>
-        <Text textAlign={'center'}>N’allez plus à la boulangerie<br />c’est elle qui vient à vous !</Text>
-        <Text>
-          Livraison à domicile, commande personnalisée, conseils de dégustation, créativité. . .
-          Ici, tout est fait main, à partir de matières premières bio, locales et nobles, pour vous proposer des pains savoureux, sains et originaux.
-        </Text>
-      </ContainerBox>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<ContainerBox>
+				<Heading as={'h1'} textAlign={'center'}>
+					Les Miches à Micha
+				</Heading>
+				<Text fontSize="2xl" textAlign={'center'}>
+					Micro-fournil artisanal
+				</Text>
+				<Text textAlign={'center'}>
+					N’allez plus à la boulangerie
+					<br />
+					c’est elle qui vient à vous !
+				</Text>
+				<Text>
+					Livraison à domicile, commande personnalisée, conseils de dégustation,
+					créativité. . . Ici, tout est fait main, à partir de matières
+					premières bio, locales et nobles, pour vous proposer des pains
+					savoureux, sains et originaux.
+				</Text>
+			</ContainerBox>
+		</Layout>
+	);
 }
