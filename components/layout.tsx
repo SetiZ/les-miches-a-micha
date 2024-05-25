d792c87d73ff786515ec92717da63b30f62bc7d9
SetiZ <funkysetiz@gmail.com>
2024-05-25 18:52:04 +0200

    "meta seo

--- components/layout.tsx
+++ components/layout.tsx
@@ -21,6 +21,15 @@ export default function Layout({ children }: LayoutProps) {
           property="og:image"
           content="https://les-miches-a-micha.vercel.app/api/og"
         />
+        <meta property="og:site_name" content="Les Miches à Micha" />
+        <meta property="og:title" content="Les Miches à Micha" />
+        <meta
+          property="og:description"
+          content="N’allez plus à la boulangerie – c’est elle qui vient à vous ! Livraison à domicile, commande personnalisée, conseils de dégustation, créativité. . . Ici, tout est fait main, à partir de matières premières bio, locales et nobles, pour vous proposer des pains savoureux, sains et originaux."
+        />
+        <meta
+          property="og:url"
+          content="https://les-miches-a-micha.vercel.app"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
