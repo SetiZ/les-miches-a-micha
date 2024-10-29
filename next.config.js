53fe724e45ef9e42cb80967a87ce8605b493057f
SetiZ <funkysetiz@gmail.com>
2024-10-29 11:53:09 +0100

    optimize package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	bundlePagesRouterDependencies: true,
+	serverExternalPackages: ['framer-motion'],
+	experimental: {
+    optimizePackageImports: ['react-icons'],
+  },
 };
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(nextConfig)
+
+// module.exports = nextConfig;
